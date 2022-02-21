cd91e63bebc15a49ff0093b52081ce97f4628732
İbrahim Halil Çadırcı <ibrahimh.cadirci@gmail.com>
2022-02-22 01:22:06 +0300

    Edits the send method in the MailGun service --continue

--- app/Services/Mail/MailGun.php
+++ app/Services/Mail/MailGun.php
@@ -3,7 +3,22 @@ namespace App\Services\Mail;
 
 
 class MailGun implements MailServiceInterface {
+
+    // Gönderen mail adresi
+    protected $mailFrom             = "ibrahimh.cadirci@sandbox89515e3cc11f47249f0224a77c99dedc.mailgun.org";
+
     public function send($subject,$message,$mailTo){
+        // First, instantiate the SDK with your API credentials
+        $mg = Mailgun::create('38eac75b97c291b2d5de6def2e84a075-b2f5ed24-f754bc03'); // For US servers
+
+        // Now, compose and send your message.
+        // $mg->messages()->send($domain, $params);
+        $mg->messages()->send('sandbox89515e3cc11f47249f0224a77c99dedc.mailgun.org', [
+            'from'    => $this->mailFrom,
+            'to'      => $mailTo,
+            'subject' => $subject,
+            'text'    => $message
+          ]);
         return response()->json([
             "message"       => "Mail Gönderildi"
         ],202);
