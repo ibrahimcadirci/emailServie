d5ea89adcf674bfb5047afe48b6b81d2bacdf0a8
İbrahim Halil Çadırcı <ibrahimh.cadirci@gmail.com>
2022-02-22 10:24:58 +0300

    Adds rules to the mail send function

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Services\Mail\MailService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MailController extends Controller
 {
     public function send(Request $request){
+        $validator = Validator::make($request->all(), [
+            'subject' => 'required|max:255',
+            'mailTo' => 'required|email',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                "message"   => "Please fill in all fields",
+                "errors" => $validator->errors()
+            ]);
+        }
+        
         $subject            = $request->input('subject');
         $message            = $request->input('message');
         $to                 = $request->input('mailTo');
